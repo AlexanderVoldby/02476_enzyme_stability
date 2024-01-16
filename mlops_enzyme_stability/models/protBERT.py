88f370fd92860b3b1e0d375d759db6204df5dd61
AlexanderVoldby <alexander@murmann.dk>
2024-01-16 15:20:26 +0100

    f*ck large files

--- mlops_enzyme_stability/models/protBERT.py
+++ mlops_enzyme_stability/models/protBERT.py
@@ -1,8 +1,11 @@
 from transformers import BertModel, BertTokenizer
-import re
+import torch
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 tokenizer = BertTokenizer.from_pretrained("Rostlab/prot_bert", do_lower_case=False)
 model = BertModel.from_pretrained("Rostlab/prot_bert")
+model = model.to(device)
+model.eval()
 
 tokenizer.save_pretrained("models/protBERT/tokenizer", from_pt=True)
 model.save_pretrained("models/protBERT/model", from_pt=True) 
