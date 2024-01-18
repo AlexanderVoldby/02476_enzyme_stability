3db4cbb0312202bc4b18ce1ed71b6a9a7764b7ac
AlexanderVoldby <alexander@murmann.dk>
2024-01-18 09:59:40 +0100

    Reverted chenages in download_BERT and removed RUN donwload_BERT from predict.dockerfile

--- mlops_enzyme_stability/data/download_BERT.py
+++ mlops_enzyme_stability/data/download_BERT.py
@@ -1,7 +1,7 @@
 from transformers import BertModel, BertTokenizer
 import hydra
 
-@hydra.main(version_base="1.3", config_name="config.yaml", config_path="../../")
+@hydra.main(version_base="1.3", config_name="config.yaml", config_path="../")
 def download_pretrained_model(config):
     tokenizer = BertTokenizer.from_pretrained("Rostlab/prot_bert", do_lower_case=False )
     model = BertModel.from_pretrained("Rostlab/prot_bert")
